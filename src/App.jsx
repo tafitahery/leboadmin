c28368e8fca5b3131846b5a7833ab2cdbaf3c37c
Tafita Hery <tafitahery@gmail.com>
2022-10-19 21:02:37 +0300

    React sidebar menu component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './app.css';
+
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
+
+function App() {
+  return (
+    <div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
