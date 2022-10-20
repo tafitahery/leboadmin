7797f2ba1eddec24894820edacd596b1f6ba34f5
Tafita Hery <tafitahery@gmail.com>
2022-10-20 14:49:34 +0300

    React Chart tutorial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+
+import { userData } from '../../dummyData';
+
 import './home.css';
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        title="User Analytics"
+        data={userData}
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 }
