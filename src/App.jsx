a562bc32bcbc10c16833b2cf074e433479cc3c8f
Tafita Hery <tafitahery@gmail.com>
2022-10-22 17:59:35 +0300

    React Material UI Table tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './app.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
