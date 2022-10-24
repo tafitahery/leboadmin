121a45b0a52e4d8f1a7a52064aa10163380f6618
Tafita Hery <tafitahery@gmail.com>
2022-10-24 17:53:39 +0300

    User display and update page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
 import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
         </Routes>
       </div>
     </BrowserRouter>
