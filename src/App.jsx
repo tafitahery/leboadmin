a74fab7ff1dd9790b42743d70cb78e24fc68d5e4
Tafita Hery <tafitahery@gmail.com>
2022-10-29 10:00:19 +0300

    Product page with reCharts

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/product/:productId" element={<User />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/newProduct" element={<NewUser />} />
         </Routes>
       </div>
